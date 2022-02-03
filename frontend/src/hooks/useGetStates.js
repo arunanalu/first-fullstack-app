8e2266ef824d7f9d0cd60165f76fd343e58aaf7f
mayusatori <satorimayu@gmail.com>
2022-02-03 16:03:39 -0300

    optimize code

---
+++ frontend/src/hooks/useGetStates.js
@@ -0,0 +1,16 @@
+import { useEffect, useState } from 'react';
+import getStates from '../services/getStates';
+
+export default function useGetStates() {
+  const [states, setStates] = useState([]);
+  
+  useEffect(() => {
+    async function requisition() {
+        const res = await getStates()
+        setStates(res)
+    }
+    requisition();
+  }, []);
+
+  return [states]
+}
--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,26 +1,18 @@
 import '../App.css';
-import React, { useEffect, useState } from 'react';
-import getStates from '../services/getStates';
+import React from 'react';
 import AppHeader from '../components/header';
+import useGetStates from '../hooks/useGetStates';
 
 function Main() {
-  const [result, setResult] = useState([]);
-
-  useEffect(() => {
-    async function requisition() {
-      const res = await getStates()
-      setResult(res)
-    }
-    requisition();
-  }, []);
-
-  // console.log(result)
+  const [states] = useGetStates();
   return (
     <div className="App">
       <AppHeader />
+      <section className="add-state-container">
+      </section>
       <section className="card-container">
         {
-          result.map((el) => {
+          states.map((el) => {
             return (
               <div className="card">
                 <p key={el._id}>{el.name}</p>
